de73eb127ad0807d1c3eb8cfffa0a3b9e0463bd3
Suraj Rajput <suraj21071998@gmail.com>
2020-08-17 17:49:19 +0530

    Used use_reducer

--- src/components/LoginComponent.js
+++ src/components/LoginComponent.js
@@ -3,13 +3,13 @@ import { Button, Form, FormGroup, Label, Input, Container,
   Card, CardBody, Row, Col, FormFeedback } from 'reactstrap';
 import PropTypes from 'prop-types';
 
-const LoginComponent = ({ username, password, setUsername, setPassword,
-    validateData, usernameError, passwordError }) => {
+const LoginComponent = ({ state, dispatch, validateData }) => {
+  let { username, password, usernameError, passwordError } = state;
   const setUsernameWrapper = (e) => {
-    setUsername(e.target.value);
+    dispatch({value: e.target.value, key: 'username'});
   }
   const setPasswordWrapper = (e) => {
-    setPassword(e.target.value);
+    dispatch({value: e.target.value, key: 'password'});
   }
   return (
     <Container>
@@ -20,17 +20,16 @@ const LoginComponent = ({ username, password, setUsername, setPassword,
               <Form>
                 <FormGroup>
                   <Label for="exampleEmail">Email</Label>
-                  <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder"
-                         value={username} onChange={setUsernameWrapper}
-                         invalid={(usernameError !== null)} />
+                  <Input type="email" name="email" id="exampleEmail"
+                    placeholder="with a placeholder" value={username}
+                    onChange={setUsernameWrapper} invalid={(usernameError !== null)} />
                   <FormFeedback>{usernameError}</FormFeedback>
                 </FormGroup>
                 <FormGroup>
                   <Label for="examplePassword">Password</Label>
                   <Input type="password" name="password" id="examplePassword"
-                         placeholder="password placeholder" value={password}
-                         onChange={setPasswordWrapper}
-                         invalid={(passwordError !== null)} />
+                    placeholder="password placeholder" value={password}
+                    onChange={setPasswordWrapper} invalid={(passwordError !== null)} />
                   <FormFeedback>{passwordError}</FormFeedback>
                 </FormGroup>
                 <Button onClick={validateData}>Submit</Button>
--- src/containers/LoginContainer.js
+++ src/containers/LoginContainer.js
@@ -1,13 +1,28 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 import LoginComponent from "../components/LoginComponent";
 import * as yup from 'yup';
-import apiHelper from "../apis/apiHelper"
+import apiHelper from "../apis/apiHelper";
+import ProductList from "../components/ProductList"
 
 const LoginContainer = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [usernameError, setUsernameError] = useState(null);
-  const [passwordError, setPasswordError] = useState(null);
+
+  const initialState = {
+    username: "",
+    password: "",
+    usernameError: null,
+    passwordError: null
+  };
+
+  function reducer(prevState, {value, key}) {
+    return {...prevState, [key]: value};
+  }
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  // const [username, setUsername] = useState("");
+  // const [password, setPassword] = useState("");
+  // const [usernameError, setUsernameError] = useState(null);
+  // const [passwordError, setPasswordError] = useState(null);
 
   let schema = yup.object().shape({
     username: yup.string().email().required(),
@@ -19,27 +34,32 @@ const LoginContainer = () => {
     //   console.log(err.name);
     //   console.log(err.errors);
     // });
-
-    schema.validate({ username, password }, { abortEarly: false })
+    schema.validate({ username: state.username, password: state.password }, { abortEarly: false })
     .then(() => {
-      apiHelper('post', 'https://api.taiga.io/api/v1/auth', {username, password, type: 'normal'}).then((response) => {
+      dispatch({value: null, key: 'usernameError'});
+      dispatch({value: null, key: 'passwordError'});
+      apiHelper('post', 'https://api.taiga.io/api/v1/auth',
+        {username: state.username, password: state.password, type: 'normal'}).then((response) => {
         console.log(response)
       })
     })
     .catch((err) => {
       err.inner.forEach((ele) => {
         // dispatch({ type: `${ele.path}Error`, value: ele.message });
-        if (ele.path === 'username') setUsernameError(ele.message);
-        if (ele.path === 'password') setPasswordError(ele.message);
-        console.log(ele.path, ele.message);
+        // if (ele.path === 'username') setUsernameError(ele.message);
+        // if (ele.path === 'password') setPasswordError(ele.message);
+        if (ele.path === 'username') dispatch({value: ele.message, key: 'usernameError'});
+        if (ele.path === 'password') dispatch({value: ele.message, key: 'passwordError'});
+        // console.log(ele.path, ele.message);
       });
     });
   };
 
   return (
-    <LoginComponent username={username}
-      password={password} setUsername={setUsername} setPassword={setPassword}
-      validateData={validateData} usernameError={usernameError} passwordError={passwordError}/>
+    // <LoginComponent username={username} password={password} setUsername={setUsername}
+    //   setPassword={setPassword} validateData={validateData} usernameError={usernameError}
+    //   passwordError={passwordError} />
+    <LoginComponent state={state} dispatch={dispatch} validateData={validateData} />
   );
 }
 
